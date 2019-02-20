980426d9d99ad2ae227de23f8634e959d4892cf5
Drazen Lucic <lucicd@hotmail.com>
2019-02-21 00:47:47 +0400

    first commit

---
+++ ConsoleTestJSON/ConsoleTestJSON/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace TestJSON
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        //Parameterless constructor, needed for serialization
+        public Person() {}
+
+        public override string ToString()
+        {
+            return string.Format("First Name: {0}\nLast Name: {1}\nAge:{2}", 
+                this.FirstName, this.LastName, this.Age);
+        }
+    }
+}
---
+++ ConsoleTestJSON/ConsoleTestJSON/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.Script.Serialization;
+
+namespace TestJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Joe", "Mango", 45);
+            Console.WriteLine(person);
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            string jsonData = js.Serialize(person);
+            Console.WriteLine(jsonData);
+
+            Console.WriteLine();
+            jsonData = "{\"FirstName\":\"Jane\",\"LastName\":\"Apple\",\"Age\":40}";
+            Console.WriteLine(jsonData);
+            Person person2 = js.Deserialize<Person>(jsonData);
+            Console.WriteLine(person2);
+
+            Console.ReadLine();
+        }
+    }
+}
